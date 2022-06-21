0c1986aa82c23803db14137cb36281e70577d9af
Emeka Daniel Umeh <100235524+Emmyd2022@users.noreply.github.com>
2022-06-21 11:01:02 +0100

    first commit

---
+++ emmylly2003/emmylly2003/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for emmylly2003 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emmylly2003.settings')
+
+application = get_asgi_application()
